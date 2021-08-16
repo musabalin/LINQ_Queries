311707c56f2bc62345be9f42299a1c4148cf3529
MUSA BALIN <20164753049@ogr.akdeniz.edu.tr>
2021-08-16 23:49:46 +0300

    New query

--- LINQ_Queries1/Form1.Designer.cs
+++ LINQ_Queries1/Form1.Designer.cs
@@ -34,14 +34,20 @@ namespace LINQ_Queries1
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.Rb5 = new System.Windows.Forms.RadioButton();
+            this.RB6 = new System.Windows.Forms.RadioButton();
+            this.RB7 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(440, 312);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(341, 283);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Linq Entity";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,29 +55,32 @@ namespace LINQ_Queries1
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(503, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(429, 150);
             this.dataGridView1.TabIndex = 1;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.radioButton1.Location = new System.Drawing.Point(12, 168);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(153, 17);
+            this.radioButton1.Size = new System.Drawing.Size(140, 20);
             this.radioButton1.TabIndex = 2;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "CourseGrade1 less than 50";
+            this.radioButton1.Text = "Exam1 less than 50";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.radioButton2.Location = new System.Drawing.Point(12, 191);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(197, 17);
+            this.radioButton2.Size = new System.Drawing.Size(244, 20);
             this.radioButton2.TabIndex = 3;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Get Name or Surname From Textbox";
@@ -79,16 +88,83 @@ namespace LINQ_Queries1
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(415, 165);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(341, 189);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 4;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.Location = new System.Drawing.Point(12, 214);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(144, 20);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "List Passed Student";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton4.Location = new System.Drawing.Point(12, 237);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(79, 20);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "List(Join)";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Rb5
+            // 
+            this.Rb5.AutoSize = true;
+            this.Rb5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Rb5.Location = new System.Drawing.Point(12, 260);
+            this.Rb5.Name = "Rb5";
+            this.Rb5.Size = new System.Drawing.Size(125, 20);
+            this.Rb5.TabIndex = 7;
+            this.Rb5.TabStop = true;
+            this.Rb5.Text = "List(SelectMany)";
+            this.Rb5.UseVisualStyleBackColor = true;
+            // 
+            // RB6
+            // 
+            this.RB6.AutoSize = true;
+            this.RB6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.RB6.Location = new System.Drawing.Point(12, 283);
+            this.RB6.Name = "RB6";
+            this.RB6.Size = new System.Drawing.Size(138, 20);
+            this.RB6.TabIndex = 8;
+            this.RB6.TabStop = true;
+            this.RB6.Text = "First Three Student";
+            this.RB6.UseVisualStyleBackColor = true;
+            // 
+            // RB7
+            // 
+            this.RB7.AutoSize = true;
+            this.RB7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.RB7.Location = new System.Drawing.Point(12, 306);
+            this.RB7.Name = "RB7";
+            this.RB7.Size = new System.Drawing.Size(138, 20);
+            this.RB7.TabIndex = 9;
+            this.RB7.TabStop = true;
+            this.RB7.Text = "Last Three Student";
+            this.RB7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 347);
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(453, 333);
+            this.Controls.Add(this.RB7);
+            this.Controls.Add(this.RB6);
+            this.Controls.Add(this.Rb5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -96,6 +172,7 @@ namespace LINQ_Queries1
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -109,6 +186,11 @@ namespace LINQ_Queries1
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton Rb5;
+        private System.Windows.Forms.RadioButton RB6;
+        private System.Windows.Forms.RadioButton RB7;
     }
 }
 
--- LINQ_Queries1/Form1.cs
+++ LINQ_Queries1/Form1.cs
@@ -19,11 +19,13 @@ namespace LINQ_Queries1
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             Dbo_EntityFrameworkDemoEntities db = new Dbo_EntityFrameworkDemoEntities();
-            if (radioButton1.Checked == true)
+            if (radioButton2.Checked == true)
             {
                 dataGridView1.DataSource = db.Tbl_CourseGrades.Select(y => new { Name = y.CourseGrade1 }).Where(x => x.Name <= 50).ToList();
             }
+
             if (radioButton2.Checked == true)
             {
                 if (textBox1.Text != "")
@@ -40,6 +42,74 @@ namespace LINQ_Queries1
                     MessageBox.Show("Enter name or surname");
                 }
             }
+            if (radioButton3.Checked == true)
+            {
+                var query = from t2 in db.Tbl_Lessons
+                            join t1 in db.Tbl_CourseGrades
+                            on t2.LessonID equals t1.Lesson
+                            join t3 in db.Tbl_Student
+                            on t1.Student equals t3.ID
+                            select new
+                            {
+                                Name = t3.FirstName + " " + t3.LastName,
+                                Lesson = t2.LessonName,
+                                Average = t1.Average,
+                                Pass = t1.Average > 50
+
+                            };
+                dataGridView1.DataSource = query.ToList();
+
+            }
+            if (radioButton4.Checked == true)
+            {
+                var result = from t1 in db.Tbl_Lessons
+                             join t2 in db.Tbl_CourseGrades
+                             on t1.LessonID equals t2.Lesson
+                             join t3 in db.Tbl_Student
+                             on t2.Student equals t3.ID
+                             select new
+                             {
+                                 StundentID = t3.ID,
+                                 Name = t3.FirstName + " " + t3.LastName,
+                                 Lesson = t1.LessonName,
+                                 CourseGrade1 = t2.CourseGrade1,
+                                 CourseGrade2 = t2.CourseGrade2,
+                                 Average = t2.Average,
+                                 IsPassed = t2.Status == true ? "successful" : "unsuccessful"
+                             };
+                dataGridView1.DataSource = result.ToList();
+            }
+            if (Rb5.Checked == true)
+            {
+                var result = db.Tbl_CourseGrades.SelectMany(x => db.Tbl_Student.Where(y => y.ID == x.Student),
+                    (x, y) => new {adi= y.FirstName, x.CourseGrade1, x.CourseGrade2, x.Average });
+                dataGridView1.DataSource = result.ToList();
+            }
+            if (RB6.Checked==true)
+            {
+                var result = db.Tbl_Student.Take(3).ToList();
+                dataGridView1.DataSource = result;
+
+            }
+            if (RB7.Checked == true)
+            {
+                var result = db.Tbl_Student.OrderByDescending(x=>x.ID).Take(3).ToList();
+                dataGridView1.DataSource = result;
+
+            }
+
+
+
+
+
+            //else
+            //{
+            //    MessageBox.Show("Choose Action", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
